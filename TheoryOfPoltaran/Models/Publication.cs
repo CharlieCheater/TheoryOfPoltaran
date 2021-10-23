7938ebbead006558024fa74a72db3b28c408e8cc
Charlie Cheater <charliecheaters@yandex.ru>
2021-10-23 09:18:03 +0300

    Обновил модель

--- TheoryOfPoltaran/Models/Publication.cs
+++ TheoryOfPoltaran/Models/Publication.cs
@@ -9,6 +9,8 @@ namespace TheoryOfPoltaran.Models
     {
         public int Id { get; set; }
         public string Title { get; set; }
+        public string Description { get; set; }
         public string Text { get; set; }
+        public DateTime Date { get; set; }
     }
 }
